43680101d93adf456cbcff301ad17eebd1d55cc3
lianghanzhong <wyattliang@gmail.com>
2026-03-11 20:30:33 +0800

    feat: add shortcuts

--- lib/pages/project/project_toolbar.dart
+++ lib/pages/project/project_toolbar.dart
@@ -5,7 +5,29 @@ import 'package:styled_widget/styled_widget.dart';
 import '../../play_controller.dart';
 import 'expansible_button.dart';
 
-class _TogglePlayIntent extends Intent {}
+class _TogglePlayIntent extends Intent {
+  const _TogglePlayIntent();
+}
+
+class _DeltaPositionIntent extends Intent {
+  const _DeltaPositionIntent(this.delta);
+  final Duration delta;
+}
+
+class _DeltaVolumeIntent extends Intent {
+  const _DeltaVolumeIntent(this.delta);
+  final double delta;
+}
+
+class _DeltaSpeedIntent extends Intent {
+  const _DeltaSpeedIntent(this.delta);
+  final double delta;
+}
+
+class _DeltaPitchIntent extends Intent {
+  const _DeltaPitchIntent(this.delta);
+  final int delta;
+}
 
 class ProjectToolbar extends StatelessWidget {
   const ProjectToolbar({required this.playController, super.key});
@@ -19,7 +41,10 @@ class ProjectToolbar extends StatelessWidget {
               ListenableBuilder(
                 listenable: playController.isPlayNotifier,
                 builder: (context, child) => FilledButton(
-                  onPressed: Actions.handler(context, _TogglePlayIntent()),
+                  onPressed: Actions.handler(
+                    context,
+                    const _TogglePlayIntent(),
+                  ),
                   style: FilledButton.styleFrom(
                     shape: const CircleBorder(),
                     padding: const EdgeInsets.all(18.0),
@@ -119,17 +144,68 @@ class ProjectToolbar extends StatelessWidget {
             .toRow(separator: const SizedBox(width: 8.0))
             .padding(horizontal: 12.0, bottom: 12.0);
 
+    return _createShortcuts(content);
+  }
+
+  Widget _createShortcuts(Widget child) {
     return Actions(
       actions: {
         _TogglePlayIntent: CallbackAction<_TogglePlayIntent>(
           onInvoke: (intent) => _togglePlayPause(),
         ),
+        _DeltaPositionIntent: CallbackAction<_DeltaPositionIntent>(
+          onInvoke: (intent) {
+            final current = playController.positionNotifier.value;
+            return playController.seekTo(current + intent.delta);
+          },
+        ),
+        _DeltaVolumeIntent: CallbackAction<_DeltaVolumeIntent>(
+          onInvoke: (intent) {
+            final current = playController.volumeNotifier.value;
+            playController.setVolume(current + intent.delta);
+            return null;
+          },
+        ),
+        _DeltaSpeedIntent: CallbackAction<_DeltaSpeedIntent>(
+          onInvoke: (intent) {
+            final current = playController.speedNotifier.value;
+            final next = (current + intent.delta).clamp(0.25, 2.0).toDouble();
+            playController.setSpeed(next);
+            return null;
+          },
+        ),
+        _DeltaPitchIntent: CallbackAction<_DeltaPitchIntent>(
+          onInvoke: (intent) {
+            final current = playController.pitchNotifier.value;
+            final next = (current + intent.delta).clamp(-7, 7);
+            playController.setPitch(next);
+            return null;
+          },
+        ),
       },
       child: Shortcuts(
         shortcuts: {
-          SingleActivator(LogicalKeyboardKey.space): _TogglePlayIntent(),
+          SingleActivator(LogicalKeyboardKey.space): const _TogglePlayIntent(),
+          SingleActivator(LogicalKeyboardKey.arrowUp): _DeltaVolumeIntent(0.1),
+          SingleActivator(LogicalKeyboardKey.arrowDown): _DeltaVolumeIntent(
+            -0.1,
+          ),
+          SingleActivator(LogicalKeyboardKey.arrowLeft): _DeltaPositionIntent(
+            Duration(seconds: -10),
+          ),
+          SingleActivator(LogicalKeyboardKey.arrowRight): _DeltaPositionIntent(
+            Duration(seconds: 10),
+          ),
+          SingleActivator(LogicalKeyboardKey.comma): _DeltaSpeedIntent(-0.25),
+          SingleActivator(LogicalKeyboardKey.period): _DeltaSpeedIntent(0.25),
+          SingleActivator(LogicalKeyboardKey.bracketLeft): _DeltaPitchIntent(
+            -1,
+          ),
+          SingleActivator(LogicalKeyboardKey.bracketRight): _DeltaPitchIntent(
+            1,
+          ),
         },
-        child: Focus(autofocus: true, child: content),
+        child: Focus(autofocus: true, child: child),
       ),
     );
   }
